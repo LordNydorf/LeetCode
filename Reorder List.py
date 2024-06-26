c5e79c051860f1428616c08f5017d20dd5fc3232
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2024-06-27 01:14:20 +0530

    Create Reorder List.py
    https://leetcode.com/problems/reorder-list/

---
+++ Reorder List.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        queue = collections.deque()
+        currentNode = head.next
+        while currentNode:
+            queue.append(currentNode)
+            currentNode = currentNode.next        
+        currentNode = head
+        while len(queue) > 1: 
+            currentNode.next = queue.pop()
+            currentNode = currentNode.next
+            currentNode.next = queue.popleft()
+            currentNode = currentNode.next
+        if queue:
+            currentNode.next = queue.pop()
+            currentNode = currentNode.next        
+        currentNode.next = None
+        return head
