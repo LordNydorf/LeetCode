5b60fadc9b385254a8f5ad620a9b7fdc85334cec
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2024-09-12 06:53:23 +0530

    Create Find the Difference.py
    https://leetcode.com/problems/find-the-difference/

---
+++ Find the Difference.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        sum_t = sum(map(ord, t))
+        sum_s = sum(map(ord, s))        
+        additional_char = chr(sum_t - sum_s)
+        return additional_char
