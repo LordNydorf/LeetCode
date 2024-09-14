d68cb9d69f1ffdc29afb81cb79371caa63a4b3fb
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2024-09-14 12:19:07 +0530

    Create Last Substring in Lexicographical Order.py
    https://leetcode.com/problems/last-substring-in-lexicographical-order/

---
+++ Last Substring in Lexicographical Order.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lastSubstring(self, s: str) -> str:
+        i = 0
+        j = 1
+        k = 0
+        n = len(s)
+        while j + k < n:
+            if s[i + k] == s[j + k]:
+                k += 1
+            elif s[i + k] > s[j + k]:
+                j += k + 1
+                k = 0
+            elif s[i + k] < s[j + k]:
+                i = max(i + k + 1, j)
+                j = i + 1
+                k = 0
+        return s[i:]
