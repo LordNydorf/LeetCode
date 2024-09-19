3f27cc3e07d9b64ce7210e56aa6c41a6d6d1a72f
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2024-09-19 18:57:47 +0530

    Create Maximum Length Substring With Two Occurrences.py
    https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/

---
+++ Maximum Length Substring With Two Occurrences.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        n = len(s)
+        r = 0
+        for i in range(n + 1):
+            for j in range(i + 1,n + 1):
+                a = (s[i:j])
+                c = 0
+                for k in a:
+                    if(a.count(k) > 2):
+                        c += 1
+                        break 
+                if(c == 0):
+                    r = max(r,len(a))
+        return(r)
