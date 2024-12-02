77558c8271b73e4e9b84ffb1ad7fb2d90b504da0
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2024-12-02 22:37:17 +0530

    Create Minimum Time to Repair Cars.py
    https://leetcode.com/problems/minimum-time-to-repair-cars/

---
+++ Minimum Time to Repair Cars.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canRepair(self, ranks, val, cars):
+        repaired = 0
+        for rank in ranks:
+            car = int(math.sqrt(val // rank))
+            repaired += car 
+        if val == 16:
+            print(repaired)
+        return repaired >= cars
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        mintime, maxtime = 0, max(ranks) * cars * cars
+        final = 0
+        while mintime <= maxtime:
+            mid = (mintime+maxtime) // 2
+            if self.canRepair(ranks, mid, cars):
+                final = mid
+                maxtime = mid - 1
+            else:
+                mintime = mid + 1
+        return final
