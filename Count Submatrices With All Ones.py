7fdb2421480cb42261ff01170026ed57ddaba7e7
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2025-04-13 15:16:17 +0530

    Create Count Submatrices With All Ones.py
    https://leetcode.com/problems/count-submatrices-with-all-ones/

---
+++ Count Submatrices With All Ones.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] and i > 0: 
+                    mat[i][j] += mat[i-1][j]
+        ans = 0
+        for i in range(m):
+            stack = []
+            cnt = 0
+            for j in range(n):
+                while stack and mat[i][stack[-1]] > mat[i][j]: 
+                    jj = stack.pop()
+                    kk = stack[-1] if stack else -1
+                    cnt -= (mat[i][jj] - mat[i][j])*(jj - kk)
+                cnt += mat[i][j]
+                ans += cnt
+                stack.append(j)
+        return ans
