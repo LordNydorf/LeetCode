a102a0e86f8e7ce7cbc67e1bbcbcbfdf373e98f1
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2025-04-22 12:57:51 +0530

    Create Array Nesting.py
    https://leetcode.com/problems/array-nesting/

---
+++ Array Nesting.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arrayNesting(self, nums: List[int]) -> int:
+        ans = 0
+        for x in nums:
+            if x == -1: continue
+            cnt = 0
+            while nums[x] != -1:
+                cnt += 1
+                nums[x], x = -1, nums[x]
+            ans = max(ans, cnt)
+        return ans
