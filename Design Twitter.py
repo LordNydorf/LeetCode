405b6aab126f822f7174759520b847cbcd21c818
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2025-05-20 23:57:22 +0530

    Create Design Twitter.py
    https://leetcode.com/problems/design-twitter/

---
+++ Design Twitter.py
@@ -0,0 +1,27 @@
+from collections import defaultdict, deque
+
+class Twitter:
+
+    def __init__(self):
+        self.follows = defaultdict(set)
+        self.feed = deque()
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.feed.appendleft((userId, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        return [tweetId for user, tweetId in self.feed if userId == user or user in self.follows[userId]][:10]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].discard(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
