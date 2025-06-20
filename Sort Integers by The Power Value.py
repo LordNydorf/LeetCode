b5aab76f0246ed7a4b2f9096e6c588ac468af512
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2025-06-21 03:46:28 +0530

    Create Sort Integers by The Power Value.py
    https://leetcode.com/problems/sort-integers-by-the-power-value/

---
+++ Sort Integers by The Power Value.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        @cache
+        def f(x):
+            if x == 1:
+                return 0
+            if not x % 2:
+                return 1 + f(x // 2)
+            else:
+                return 1 + f(3 * x + 1)
+        res = []
+        for i in range(lo, hi + 1):
+            res.append((i, f(i)))
+        res.sort(key = lambda x:x[1])
+        return res[k-1][0]       
