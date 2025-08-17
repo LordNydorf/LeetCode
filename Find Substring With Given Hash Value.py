c2b5df5fe00eb6e62129c27d66e9d6297243535c
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2025-08-17 10:23:47 +0400

    Create Find Substring With Given Hash Value.py
    https://leetcode.com/problems/find-substring-with-given-hash-value/

---
+++ Find Substring With Given Hash Value.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
+        n = len(s)
+        val = lambda v: ord(v) - ord("a") + 1
+        powers = [pow(power, i, modulo) for i in range(k)]
+        idx = n - k
+        hash_val = 0
+        for i in range(k):
+            hash_val += val(s[n - k + i]) * powers[i]
+        for i in range(n - k - 1, -1, -1):
+            hash_val = ((hash_val - val(s[i + k]) * powers[k - 1]) * power + val(s[i])) % modulo
+            if hash_val == hashValue:
+                idx = i
+        return s[idx : idx + k]
