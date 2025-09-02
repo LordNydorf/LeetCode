037646c1b7bc2cf7134efcc4147cb9721684d6a8
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2025-09-03 01:25:54 +0400

    Create Find Resultant Array After Removing Anagrams.py
    https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/

---
+++ Find Resultant Array After Removing Anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        def are_anagrams(s1: str, s2: str) -> bool:
+            count = [0] * 26
+            for c in s1:
+                count[ord(c) - ord('a')] += 1
+            for c in s2:
+                count[ord(c) - ord('a')] -= 1
+            return all(c == 0 for c in count)
+        i = 1
+        while i < len(words):
+            if are_anagrams(words[i-1], words[i]):
+                del words[i]
+            else:
+                i += 1
+        return words
